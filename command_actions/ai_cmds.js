2064382f095b64b47a2ccde63c490c3da39ac9b2
Dang Nguyen <dangns229@gmail.com>
2025-11-26 15:13:20 -0800

    big updates

---
+++ command_actions/ai_cmds.js
@@ -0,0 +1,5 @@
+function send_ai_request(msg){
+
+}
+
+export {send_ai_request};
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+import 'dotenv/config'
 import Discord from "discord.js";
-import {execute, skip, stop, print_queue} from "./command_actions/music.js";
 import { createRequire } from 'module'
-import {new_mem_greeting, hello_greeting, image_greeting} from "./command_actions/server_greeting.js"
-
+import * as youtube_cmds from "./command_actions/youtube_cmds.js"
+import * as general_cmds from "./command_actions/general_cmds.js"
+import * as ai_cmds from "./command_actions/ai_cmds.js"
 
 const require = createRequire(import.meta.url);
 const config = require("./config.json");
+const token = process.env.DISCORD_TOKEN;
 const client = new Discord.Client();
 
 const queue = new Map();
@@ -25,33 +27,44 @@ client.once("disconnect", () => {
 client.on("message", (msg) => {
   if (msg.author.bot) return;
   if (!msg.content.startsWith(config.prefix)) return;
-  
-  if (msg.content ===`${config.prefix}hello`) {
-    msg.reply("hello onichan!");
-  }
 
-  if (msg.content.startsWith === `${config.prefix}image`) {
-    image_greeting(msg, storage)
+  //#region General Commands
+  if (msg.content ===`${config.prefix}hi`) {
+    general_cmds.hello_greeting(msg);
     return;
   }
+  if (msg.content.startsWith === `${config.prefix}image`) {
+    general_cmds.image_greeting(msg, storage)
+    return;
+  }
+  //#endregion
 
+  //#region Youtube Commands
   const serverQueue = queue.get(msg.guild.id);
-
   if (msg.content.startsWith(`${config.prefix}play`)) {
-    execute(msg, queue, serverQueue);
+    youtube_cmds.execute(msg, queue, serverQueue);
     return;
   } else if (msg.content.startsWith(`${config.prefix}skip`)) {
-    skip(msg, serverQueue);
+    youtube_cmds.skip(msg, serverQueue);
     return;
   } else if (msg.content.startsWith(`${config.prefix}stop`)) {
-    stop(msg, serverQueue);
+    youtube_cmds.stop(msg, serverQueue);
     return;
   } else if (msg.content.startsWith(`${config.prefix}queue`)) {
-    print_queue(msg, serverQueue);
+    youtube_cmds.print_queue(msg, serverQueue);
     return;    
   } else {
     msg.channel.send("You need to enter a valid command!");
   }
+  //#endregion
+
+  //#region AI Commands
+  if (msg.content.startsWith(`${config.prefix}ai`)) {
+    ai_cmds.send_ai_request(msg);
+    return;
+  }
+  //#endregion
+
 });
 
-client.login(`${config.token}`);
+client.login(`${token}`);
